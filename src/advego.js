f20a031c919e926a0db38ba8f7f6b55548790a1b
flasharium <dmitrygrand@gmail.com>
2014-12-30 17:43:35 +0300

    add advego script

---
+++ src/advego.js
@@ -0,0 +1,22 @@
+$(function(){
+	if (window.location.href == "http://advego.ru/text/seo/") {
+
+		$('head').append($('<style>' +
+			'.selected {background-color: #9999ff !important;}' +
+		'</style>'));
+
+		$('body').append($('<textarea id="sdlhfuifhieswfbhuwf7234rh8327hr4" style="position: fixed;top:23px;left:20px;width: 315px;height: 465px;"></textarea>'))
+
+		$('#text_check_results > div:nth-child(4) > table > tbody > tr > td:nth-child(1)').each(function(){
+			$(this).click(function(){
+				$(this).toggleClass('selected');
+				var str = [];
+				$('#text_check_results > div:nth-child(4) > table > tbody > tr > td:nth-child(1).selected').each(function(){
+					str.push($(this).text())
+				})
+				$('#sdlhfuifhieswfbhuwf7234rh8327hr4').val(str.join(' '));
+			})
+		})
+	}
+});
+
